e8bb174e5caf2e5f1d5b3faeba310d500c607619
daviClimaco <davibomfimclimaco@gmail.com>
2026-06-29 01:35:11 -0300

    feat: Add Category model

---
+++ src/models/category.py
@@ -0,0 +1,15 @@
+from sqlalchemy import String, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql import func
+
+from database.base import Base
+
+class Category(Base):
+
+    __tablename__ = "category"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    name: Mapped[str] = mapped_column(String(100),nullable=False,unique=True)
+
+    created_at: Mapped[DateTime] = mapped_column(DateTime,server_default=func.now())
