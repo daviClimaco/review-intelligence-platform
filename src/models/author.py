4f712a79491b5c03eca1a2d22ed60ff4b9a599fc
daviClimaco <davibomfimclimaco@gmail.com>
2026-06-29 01:30:34 -0300

    feat: Add base.py for Postgres 18 support via SQLAlchemy and Author model

---
+++ src/models/author.py
@@ -0,0 +1,15 @@
+from sqlalchemy import String, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql import func
+
+from database.base import Base
+
+class Author(Base):
+
+    __tablename__ = "author"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    name: Mapped[str] = mapped_column(String(150),nullable=False)
+
+    created_at: Mapped[DateTime] = mapped_column(DateTime,server_default=func.now())
